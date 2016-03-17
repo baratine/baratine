3953f3918b2bdbfc7aefecf475a792bad681565b
alex <rojkov@gmail.com>
2016-03-16 17:42:10 -0700

    revert import order

--- api/src/main/java/io/baratine/web/Body.java
+++ api/src/main/java/io/baratine/web/Body.java
@@ -29,12 +29,12 @@
 
 package io.baratine.web;
 
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
 @Retention(RUNTIME)
