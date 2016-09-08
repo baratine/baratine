4cdc2a410bc77fab29ccb8e9243bc51d5fceb5ab
alex <rojkov@gmail.com>
2016-09-08 10:36:30 -0700

    priority javadoc updates

--- core/src/main/java/com/caucho/v5/config/Priority.java
+++ core/src/main/java/com/caucho/v5/config/Priority.java
@@ -5,13 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 
+/**
+ * Annotation Priority is used to determine order of matching bindings for
+ * Beans, ViewResolvers, ViewRenders, Converters
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({TYPE,METHOD})
 public @interface Priority
 {
+  /**
+   * Priority value for selecting the top most match
+   *
+   * @return priority value
+   */
   int value();
 }
